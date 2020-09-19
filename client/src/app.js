c9d6e982a5cdb81a912081f9e2956a53de12846a
Haroon Syed <51543523+haroonsyed@users.noreply.github.com>
2020-09-19 19:42:36 -0400

    Some clean-up and content wrapping in app.js before adding rest of front end

--- client/src/app.js
+++ client/src/app.js
@@ -6,43 +6,48 @@ import Friends from "./components/friends";
 import MyProfile from "./components/my-profile";
 import Resources from "./components/resources";
 import Login from "./components/login";
-import InputBox from "./components/InputBox";
+import { Layout } from './components/layout';
+import { Jumbotron } from './components/Jumbotron';
 
 class App extends Component {
 
     render() {
         return (
-            <Router>
-                <div className="container">
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <div className="collpase navbar-collapse">
-                            <ul className="navbar-nav mr-auto">
-                                <li className="navbar-item">
-                                    <Link to="/" className="navbar-brand">COVID Connect</Link>
-                                </li>
-                                <li className="navbar-item">
-                                    <Link to="/friend" className="nav-link">Friends</Link>
-                                </li>
-                                <li className="navbar-item">
-                                    <Link to="/profile" className="nav-link">My Profile</Link>
-                                </li>
-                                <li className="navbar-item">
-                                    <Link to="/resources" className="nav-link">COVID Resources</Link>
-                                </li>
-                                <li className="navbar-item">
-                                    <Link to="/button" className="nav-link">Login</Link>
-                                </li>
-                            </ul>
+            <React.Fragment>
+                <Layout>
+                    <Router>
+                        <div className="container">
+                            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                                <div className="collpase navbar-collapse">
+                                    <ul className="navbar-nav mr-auto">
+                                        <li className="navbar-item">
+                                            <Link to="/" className="navbar-brand">COVID Connect</Link>
+                                        </li>
+                                        <li className="navbar-item">
+                                            <Link to="/friend" className="nav-link">Friends</Link>
+                                        </li>
+                                        <li className="navbar-item">
+                                            <Link to="/profile" className="nav-link">My Profile</Link>
+                                        </li>
+                                        <li className="navbar-item">
+                                            <Link to="/resources" className="nav-link">COVID Resources</Link>
+                                        </li>
+                                        <li className="navbar-item">
+                                            <Link to="/button" className="nav-link">Login</Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
                         </div>
-                    </nav>
-                </div>
-                <br/>
-                <Route path="/" exact component={Home} />
-                <Route path="/friend" component={Friends} />
-                <Route path="/profile" component={MyProfile} />
-                <Route path="/resources" component={Resources} />
-                <Route path="/button" component={Login} />
-            </Router>
+                        <br/>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/friend" component={Friends} />
+                        <Route path="/profile" component={MyProfile} />
+                        <Route path="/resources" component={Resources} />
+                        <Route path="/button" component={Login} />
+                    </Router>
+                </Layout>
+            </React.Fragment>
         );
     }
 }
---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Jumbotron as Jumbo, Container} from 'react-bootstrap';
+
+export const Jumbotron = () => (
+    <Jumbo>
+        <div className="overlay"></div>
+        <Container>
+            
+        </Container>
+    </Jumbo>
+);
